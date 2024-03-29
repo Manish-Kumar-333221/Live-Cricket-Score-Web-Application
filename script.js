35094e40dc6cb7b0f21b9c889f385dd288aae424
SUKHDEV PAUL <sukhdevpaul01@gmail.com>
2024-03-29 10:10:16 +0530

    few updatea from my side

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-//this code is use for menu button slide movement in frontend
+//this scripts is use for css menu button from here start here
+function toggleMenu() {
+    var menu = document.getElementById('main-menu');
+    menu.classList.toggle('open');
+}
 
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.getElementById('menu-toggle');
-    const slidingMenu = document.getElementById('sliding-menu');
-
-    menuToggle.addEventListener('click', function () {
-            console.log("Runnig 2")
-        slidingMenu.classList.toggle('show-menu');
-    });
-
-    document.addEventListener('click', function (event) {
-        const isClickInsideMenu = slidingMenu.contains(event.target);
-        const isClickInsideToggle = menuToggle.contains(event.target);
-        if (!isClickInsideMenu && !isClickInsideToggle) {
-            slidingMenu.classList.remove('show-menu');
-        }
-    });
+// Close menu when clicking outside
+window.addEventListener('click', function(e) {
+    var menu = document.getElementById('main-menu');
+    var menuBtn = document.querySelector('.menu-btn');
+    if (!menu.contains(e.target) && !menuBtn.contains(e.target)) {
+        menu.classList.remove('open');
+    }
 });
+
+
+//nav2 horizontal 
+document.getElementById('showIPL').addEventListener('click', function() {
+    document.getElementById('ipl2024').classList.toggle('open');
+    document.getElementById('ipl2024').classList.toggle('slide-up');
+});
+
+window.addEventListener('click', function(event) {
+    if (!document.getElementById('ipl2024').contains(event.target) && event.target != document.getElementById('showIPL')) {
+        document.getElementById('ipl2024').classList.remove('open');
+        document.getElementById('ipl2024').classList.add('slide-up');
+    }
+});
+
+
+//to css script end here!!
